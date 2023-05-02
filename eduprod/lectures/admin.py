b6c5c557f548bfe58e6c6bf881e406a76cfa294f
Daulet Serikuly <67403963+DauletSerikuly@users.noreply.github.com>
2023-05-02 15:56:40 +0600

    Delete eduprod directory

--- eduprod/lectures/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Lecture
-from tests.models import Test
-
-admin.site.register(Lecture)
-admin.site.register(Test)
--- eduprod/tests/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import test_list, test_detail
-
-urlpatterns = [
-    path('tests/', test_list, name='test_list'),
-    path('tests/<int:pk>/', test_detail, name='test_detail'),
-]
